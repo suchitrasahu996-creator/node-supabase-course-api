c1b5ed6b9aaa6d8c7ca47a8a5bbc3d326c65a387
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-03-08 15:29:53 +0530

    adding the answer

--- course-enrollment-api/src/middleware/validateEnrollment.js
+++ course-enrollment-api/src/middleware/validateEnrollment.js
@@ -2,7 +2,9 @@ const validateEnrollment =(req,res,next)=>{
     const {student_name ,course_id}= req.body;
     if (!student_name || !course_id ) {
         return res.status(400).json({
-            error:"student"
-        })
+            error:"student_name and course_id are required";
+
+        });
     }
+    next();
 }
