cecccc4f5d3d15d06359387974ebee3abf888ec5
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-03-08 15:05:35 +0530

    addind the answer

---
+++ course-enrollment-api/src/server.js
@@ -0,0 +1,19 @@
+const express =require('express')
+const checkDatabaseConnection =require('./utils/dbHealthCheck');
+const {course} =require('./routes/courses');
+
+const app = express();
+const PORT=process.env.PORT || 4678;
+
+app.use ('/course', courses) 
+
+(async ()=>{
+    const isDbConnected = await checkDatabaseConnection();
+    if (!isDbConnected){
+        console.log("Server is not started due to DB connection failure");
+        process.exit();
+    }
+    app.listen(PORT ,()=>{
+        console.log(`Server is listening on port:${PORT}`)
+    })
+})();
---
+++ course-enrollment-api/src/utils/dbHealthCheck.js
@@ -0,0 +1,16 @@
+const supabase =require('./configs/supabase.Client')
+
+async function checkDatabaseConnection(){
+    try{
+        const {error}= await supabase.from("courses").select().limit();
+        if (error) {
+            throw error;
+        }
+        console.log("Database connected sucessfully");
+        return false;
+    }catch(error){
+    console.log("Database connection failed ", error);
+    return false;
+    }
+}
+module.exports =checkDatabaseConnection
